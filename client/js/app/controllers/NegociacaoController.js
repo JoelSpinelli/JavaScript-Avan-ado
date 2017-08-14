04c1fbb5307881614ab65fbb7396cc434604dc1f
Joel Spinelli <joel.spinelli@hotmail.com>
2017-08-14 00:43:27 -0300

    Criando e trabalhando com DateHelper

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,20 +10,16 @@ class NegociacaoController{
 
     adiciona(event){
         event.preventDefault();
+        let helper = new DateHelper();
         
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
         
         let negociacao = new Negociacao(
-            data,
+            helper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
         console.log(negociacao);
-        //adicionar a negociacao em uma lista
 
+        console.log(helper.dataParaTexto(negociacao.data));
     }
 }
