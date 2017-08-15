dd007ab8ccaca8aa9799acd3ea9fc97278edf2a3
Joel Spinelli <joel.spinelli@hotmail.com>
2017-08-15 00:47:13 -0300

    Colocando validações no DateHelper

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -4,12 +4,14 @@ class DateHelper {
         throw new Error('Esta classe não pode ser instanciada');
     }
 
-    static textoParaData(texto){
-        return new Date(... texto.split('-').map((item, indice) => item - indice % 2));
+    static dataParaTexto(data){
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`
     }
 
-    static dataParaTexto(data){
-        return data.getDate() + '/' + (data.getMonth()+1) + '/' + data.getFullYear();
-        
+    static textoParaData(texto){
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+
+        return new Date(... texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
